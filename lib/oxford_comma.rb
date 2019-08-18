2c72af8f261a295b8cbc4b6a2b1d88bedf9ebe49
Donovan Jones <donovanljones94@gmail.com>
2019-08-18 19:00:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    puts array.to_s
+  elsif array.length == 2
+    puts array.join('and')
+  elsif array.length >= 3
+    last_item = array[-1]
+    new_item = "and #{last_item}"
+    array.pop
+    array.push(new_item)
+    puts array.join(',')
+  end
 end
